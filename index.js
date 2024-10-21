9954ff956a4878d6862e095161732c2a0d03ae4d
Sebastian Skiba <sebastianskiba@MacBook-Pro-Sebastian.local>
2024-10-21 22:27:09 +0200

    Pierwsza modyfikacja

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// app.get('/api/', (req, res) => {
+//     res.send("Siema");
+// })
+
 // ADD TEAM
 
 app.get('/api/getteam', (req, res) => {
@@ -50,6 +54,7 @@ app.get('/api/getteam', (req, res) => {
     "SELECT * FROM team";
 
     db.query(sqlSelect, (err, result) => {
+        // console.log(err);
         res.send(result);
     })
 })
